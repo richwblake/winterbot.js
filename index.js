3ba51cd682d807fccb171c0e26f76ede8c45a45f
wills blake <richwblake@gmail.com>
2022-09-18 19:11:54 -0700

    cleans comments from index

--- index.js
+++ index.js
@@ -30,34 +30,4 @@ for (const file of eventFiles) {
     }
 }
 
-// client.once('ready', c => {
-//     console.log(`Logged in as ${c.user.tag} and ready to go!`);
-// });
-// 
-// client.on('interactionCreate', interaction => {
-//     console.log(`${interaction.user.tag} in ${interaction.channel.name} triggered an interaciton!`);
-// });
-// client.on('interactionCreate', async interaction => {
-//     if (!interaction.isChatInputCommand()) return;
-// 
-//     const command = interaction.client.commands.get(interaction.commandName);
-// 
-//     if (!command) return;
-// 
-//     try {
-//         await command.execute(interaction);
-//     } catch (error) {
-//         console.log(error);
-//         await interaction.reply({ content: 'An error occurred, sorry about that!', ephemeral: true });
-//     }
-// 
-//     if (commandName === 'ping') {
-//         await interaction.reply('Pong!');
-//     } else if (commandName === 'server') {
-//         await interaction.reply(`Server name: ${interaction.guild.name}\nMember count: ${interaction.guild.memberCount}`);
-//     } else if (commandName === 'user') {
-//         await interaction.reply(`Your username: ${interaction.user.username}\nYour user ID: ${interaction.user.id}`);
-//     }
-// });
-
 client.login(TOKEN);
